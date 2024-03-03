a6ea6e054a0700f5608ff708d9d10f8b6af60a6f
sayechu <santiayechu@gmail.com>
2024-03-03 22:10:45 +0100

    add setTimeout() functionality

--- practica05/ejercicio5.js
+++ practica05/ejercicio5.js
@@ -1,4 +1,7 @@
 let minNum = 0;
 let maxNum = 10;
 
-let num = (Math.random() * (maxNum-minNum) + minNum);
+let num = Math.floor(Math.random() * (maxNum-minNum) + minNum) * 1000;
+//console.log(num);
+
+setTimeout(() => { window.open("https://www.w3schools.com"); }, num);
