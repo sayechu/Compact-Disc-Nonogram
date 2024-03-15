6cbddb9d0fd468ac7d82991cbf5af0764166fda1
Santi Ayechu <santiayechu@gmail.com>
2024-03-15 21:07:15 +0100

    clean directories + remove useless directories

--- practica05/ejercicio2.js
+++
@@ -1,7 +0,0 @@
-let nombre = window.prompt("Introduce tu nombre: ");
-let edad = window.prompt("Introduce tu edad: ");
-let profesion = window.prompt("Introduce tu profesion: ");
-
-para = document.createElement("p");
-para.innerText = `Nombre: ${nombre}; Edad: ${edad}; Profesion: ${profesion}`;
-document.body.appendChild(para);
--- practica05/ejercicio5.js
+++
@@ -1,7 +0,0 @@
-let minNum = 0;
-let maxNum = 10;
-
-let num = Math.floor(Math.random() * (maxNum-minNum) + minNum) * 1000;
-//console.log(num);
-
-setTimeout(() => { window.open("https://www.w3schools.com"); }, num);
